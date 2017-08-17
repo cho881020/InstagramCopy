8a99a99131ce2afc7181de22202b64e1469d7427
cho881020 <cho881020@gmail.com>
2017-08-17 17:41:46 +0900

    포스트 상세 조회 화면 그림.

---
+++ app/src/main/java/kr/co/tjeit/instagramcopy/ViewPostActivity.java
@@ -0,0 +1,31 @@
+package kr.co.tjeit.instagramcopy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewPostActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_post);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
